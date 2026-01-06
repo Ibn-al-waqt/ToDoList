d54d4503fd0a569c8052353ad9d5a3e0b2e5aa6d
sherka <154922248+sherka-57@users.noreply.github.com>
2026-01-06 10:01:12 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -48,12 +48,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/todos", todoRoutes);
 
 // SPA fallback 
-app.get('/:all(*)', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 
