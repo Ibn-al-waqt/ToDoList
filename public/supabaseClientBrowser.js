b2d3e41abcaf2d62777baf4035df377326712daa
sherka <154922248+sherka-57@users.noreply.github.com>
2026-01-08 12:26:46 -0500

    Update supabaseClientBrowser.js

--- public/supabaseClientBrowser.js
+++ public/supabaseClientBrowser.js
@@ -3,5 +3,11 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 export const supabase = createClient(
   "https://sazdtrcayjljgmihilkj.supabase.co",        // replace with your URL
-  "sb_publishable_8PjevXnceWHX2i_xoB-M7g_UYzHxq5c"            // replace with your anon/publishable key
+  "sb_publishable_8PjevXnceWHX2i_xoB-M7g_UYzHxq5c",            // replace with your anon/publishable key
+  {
+    auth: {
+      persistSession: true,
+      storage: sessionStorage,
+    }
+  }
 )
